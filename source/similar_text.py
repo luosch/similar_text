97ef3a80d7fca3b74442e76d25d8dd3a2fc2c923
luosch <me@lsich.com>
2015-12-31 00:50:12 +0800

    add fast option via edit distance~

--- source/similar_text.py
+++ source/similar_text.py
@@ -7,9 +7,8 @@ def similar_str(str1, str2):
     """
     max_len = tmp = pos1 = pos2 = 0
     len1, len2 = len(str1), len(str2)
-    # use str1 as base
+
     for p in range(len1):
-        # traverse str2
         for q in range(len2):
             tmp = 0
             while p + tmp < len1 and q + tmp < len2 \
@@ -40,16 +39,42 @@ def similar_char(str1, str2):
 
 
 def similar_text(str1, str2, option='normal'):
-    if option == 'normal':
+    """
+    return a float value in [0, 100], which stands for match level
+    """
+    
+    if len(str1) == 0 and len(str2) == 0:
+        return 0.0
+    elif option == 'normal':
         return similar_char(str1, str2) * 200.0 / (len(str1) + len(str2))
     elif option == 'fast':
-        pass
+        return 100.0 - edit_distance(str1, str2) * 200.0 / (len(str1) + len(str2))
     else:
         raise ValueError("no such option")
 
 
-def edit_distance():
-    pass
+def edit_distance(str1, str2):
+    m, n = len(str1) + 1, len(str2) + 1
+        
+    if m == 1 or n == 1:
+        return max(m, n) - 1
+    
+    dp = [[0 for _ in range(n)] for __ in range(m)]
+    
+    for i in range(m):
+        dp[i][0] = i
+    
+    for i in range(n):
+        dp[0][i] = i
+    
+    for i in range(1, m):
+        for j in range(1, n):
+            if str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = min(dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]) + 1
+    
+    return dp[-1][-1]
 
 # # simple test
 # if __name__ == "__main__":
--- test_similar_text.py
+++ test_similar_text.py
@@ -1,10 +1,26 @@
+# -*- coding: utf-8 -*-
+
 import unittest
 from source.similar_text import similar_text
 
 class TestStringMethods(unittest.TestCase):
     def test_similar_normal(self):
         case1 = similar_text('aaaa', 'aaaa')
+        case2 = similar_text('aaaa', 'bbbb')
+        case3 = similar_text('abcdef', 'aabcdefg', 'normal')
+
         self.assertTrue(int(case1) == 100)
+        self.assertTrue(int(case2) == 0)
+        self.assertTrue(int(case3) == 85)
+
+    def test_similar_fast(self):
+        case1 = similar_text('aaaa', 'aaaa', 'fast')
+        case2 = similar_text('aaaa', 'bbbb', 'fast')
+        case3 = similar_text('abcdef', 'aabcdefg', 'fast')
+
+        self.assertTrue(int(case1) == 100)
+        self.assertTrue(int(case2) == 0)
+        self.assertTrue(int(case3) == 71)
 
     def test_similar_option(self):
         self.assertRaises(ValueError, similar_text, 'aaaa', 'aaaa', 'very fast')
