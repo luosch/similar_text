32fec631466b2fb467dca5b163a0a9d913494b51
luosch <me@lsich.com>
2015-12-31 22:56:23 +0800

    fix readme error

--- setup.py
+++ setup.py
@@ -3,22 +3,26 @@ try:
 except:
     from distutils.core import setup
 
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+
 setup(
     name = 'similar_text',
     packages = ['source'],
     version = '0.1',
     description = 'Calculates the similarity between two strings',
-    long_description = open('README.md').read(),
+    long_description = readme,
     author = 'Sicheng Luo',
     author_email = 'i@lsich.com',
     url = 'https://github.com/luosch/similar_text',
-    download_url = 'https://github.com/luosch/similar_text/tarball/v0.1',
     keywords = ['similar_text', 'similar', 'text'],
     license = 'MIT',
     classifiers = [
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Intended Audience :: Developers',
-        'Operating System :: OS Independent'
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7'
     ]
 )
