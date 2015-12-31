1d36cae478b5f5e2e404ba51d129baa29960e9af
luosch <me@lsich.com>
2015-12-31 22:42:57 +0800

    v0.1

--- setup.py
+++ setup.py
@@ -8,13 +8,17 @@ setup(
     packages = ['source'],
     version = '0.1',
     description = 'Calculates the similarity between two strings',
-    long_description = open('README.md').read()
+    long_description = open('README.md').read(),
     author = 'Sicheng Luo',
     author_email = 'i@lsich.com',
     url = 'https://github.com/luosch/similar_text',
     download_url = 'https://github.com/luosch/similar_text/tarball/v0.1',
     keywords = ['similar_text', 'similar', 'text'],
+    license = 'MIT',
     classifiers = [
-        'License :: MIT License'
-    ],
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent'
+    ]
 )
